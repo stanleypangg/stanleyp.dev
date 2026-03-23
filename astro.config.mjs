4120db0afd8638bd9f4551167ffb634734fe98e0
Stanley Pang <stanley.pang@mail.utoronto.ca>
2026-03-22 20:49:01 -0400

    feat: add @astrojs/mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   output: 'static',
   site: 'https://example.com',
   adapter: vercel(),
+  integrations: [mdx()],
 });
